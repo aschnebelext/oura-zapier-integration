c7166599e686f47a1401167e9dd3e894376044fb
Alexander Schnebel <alexander@schnebel.dev>
2022-08-26 20:38:16 +0200

    fix: convert getPercentOf return type to number

--- utils/mathUtils.js
+++ utils/mathUtils.js
@@ -1,6 +1,6 @@
 const getPercentOf = (sub, sum) => {
   if (sub == null || (sum == null || sum === 0)) return 0;
-  return (sub / sum).toFixed(2);
+  return Number((sub / sum).toFixed(2));
 };
 
 module.exports = {
